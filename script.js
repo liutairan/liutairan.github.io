c1f6483225a29da2a87717754ea16d8eee13874f
Tairan Liu <liutairan2012@gmail.com>
2018-05-03 09:43:10 -0500

    Adjustment

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+/* Toggle between adding and removing the "responsive"
+   class to topnav when the user clicks on the icon */
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
