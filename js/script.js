b68e1ff5f408c21786cfb3292e64320d0806cfeb
Tairan Liu <liutairan2012@gmail.com>
2019-08-15 23:03:18 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,3 +8,21 @@ function myFunction() {
         x.className = "topnav";
     }
 }
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myStickyFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("myTopnav");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myStickyFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
